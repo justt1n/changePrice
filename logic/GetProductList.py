cb03f9f915729916545308bb42d7907593bbdd29
justt1n <tinnt0709@gmail.com>
2024-10-08 00:05:08 +0700

    hide key

--- logic/GetProductList.py
+++ logic/GetProductList.py
@@ -9,7 +9,7 @@ def fetch_product_list(output_file = 'products.json'):
     base_url = "https://backend.gamivo.com/api/public/v1/products"
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MjgzMTA1OTMsImV4cCI6MTc1OTg0NjU5Mywic2NvcGUiOiJwdWJsaWMiLCJpZCI6NTc1NDY4MH0.AsP9Vw5Dwdn98Pi4ClYIpPUMvoP60GVwQuTI2_lkAixNKJFY7K9ueJowzyGsWCzcCaKVM615L_LlWRD_cEOLPgGjTjZW3UBj9cyDZJVb6PgzZtL_EhKUC-rh6xgKWVMdV_oRJbKlDT5gr4DZSdbArsiAtokWZ7Y6ITqKej1XiiIQ16EOS6VXw0-hIc0YDHkvjVHzTHjcE7AX_zKoamyhHGeZSmwnov-5fBfYdj3CFVPrcjM4HARDerIK4iq9hsvXlBsd6uWwD35KD3udcJp8GVzi2uGOH1rH1zS44lOCsnRPhnOVO6bTZ27bdnahrZJaKJySlS1qvmkld_FxEJDSR-dePITaVCPE0mre3JyjU9vqdbwDorwKPxcEBULTTUgi_sId87hOanylL0scF27wy3TCZWndAt1hlIPWO7wfU1N29a9gvRvAoHzQ2Ad6ivM0oiHmpDAEsrH1SxZvXEeff5CzZUhv5cbdAjanQcjI6Xr3E7KnhbgFNmmCBEFJoLy36JHWb6604_9VpwPEXM9JiLHGUe-t0CghwVa5i4bINwJWVTrooImQg2qSPDJT65RTbaaXkJOs_rk9eo4jkBLSbnu86lnyfCnXzQFcBscHblEyWQxZW6udss7GCrOl-mhS9nCH6dvUFBnoDQFG6x0waUe2BFIOooCV2nPSVVIm6xA"
+        "Authorization": os.getenv('GAMIVO_API_KEY')
     }
 
     # Parameters for pagination
@@ -75,6 +75,6 @@ def fetch_product_list(output_file = 'products.json'):
     print(f"Data fetching complete. Results saved in {output_file}")
 
 
-def main():
+def get_product_list():
     fetch_product_list(os.getenv('OUTPUT_FILE', 'products.json'))
     migrate_json_to_sqlite(os.getenv('OUTPUT_FILE', 'products.json'), os.getenv('SQLITE_DB', 'products.db'))
