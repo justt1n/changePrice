1f866e0665f54ef9dbc756fee9c8ccadce80c78b
tinnt <tinnt@airtrip-intl.com>
2024-10-18 11:02:35 +0700

    update getFinalPrice

--- main.py
+++ main.py
@@ -198,10 +198,26 @@ def do_payload(index, payload, blacklist_cache=None):
     _current_top_offers = get_product_offers(int(payload.PRODUCT_COMPARE), os.getenv('GAMIVO_API_KEY'))
     _second_current_top_price = None
     _second_current_top_seller = None
-    if len(_current_top_offers) == 2:
+    _third_current_top_price = None
+    _third_current_top_seller = None
+    _fourth_current_top_price = None
+    _fourth_current_top_seller = None
+    if len(_current_top_offers) >= 2:
         _second_current_top_price = _current_top_offers[1]['price']
         _second_current_top_seller = _current_top_offers[1]['seller']
         _second_current_top_price = calculate_seller_price(_offer_id, _second_current_top_price,
+                                                           os.getenv('GAMIVO_API_KEY')).get(
+            'seller_price')
+    if len(_current_top_offers) >= 3:
+        _third_current_top_price = _current_top_offers[2]['price']
+        _third_current_top_seller = _current_top_offers[2]['seller']
+        _third_current_top_price = calculate_seller_price(_offer_id, _third_current_top_price,
+                                                          os.getenv('GAMIVO_API_KEY')).get(
+            'seller_price')
+    if len(_current_top_offers) >= 4:
+        _fourth_current_top_price = _current_top_offers[3]['price']
+        _fourth_current_top_seller = _current_top_offers[3]['seller']
+        _fourth_current_top_price = calculate_seller_price(_offer_id, _fourth_current_top_price,
                                                            os.getenv('GAMIVO_API_KEY')).get(
             'seller_price')
     # Skip if seller is in blacklist
@@ -247,14 +263,17 @@ def do_payload(index, payload, blacklist_cache=None):
     edit_offer_payload = price_service.convert_to_new_offer(offer_data, _target_price, stock)
     print(("offer_data", offer_data))
     print(f"Set {payload.Product_name} to {_target_price}")
-    if _current_price != _target_price:
-        status_code, res = put_edit_offer_by_id(edit_offer_payload, _offer_id, os.getenv('GAMIVO_API_KEY'))
-        if status_code == 200:
-            log_str = f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}: Giá đã cập nhật thành công; Price = {_current_price}; Pricemin = {min_price}, Pricemax = {max_price}, GiaSosanh = {_current_top_price} - Seller: {_current_top_seller}"
-            log_str += f"\n----Top Sellers----\n- 1st Seller: {_current_top_seller} - Price: {_current_top_price}"
-            if (_second_current_top_seller is not None) and (_second_current_top_price is not None):
-                log_str += f"\n- 2nd Seller: {_second_current_top_seller} - Price: {_second_current_top_price}"
-            log_data.append((index, log_str, 'C'))
+    status_code, res = put_edit_offer_by_id(edit_offer_payload, _offer_id, os.getenv('GAMIVO_API_KEY'))
+    if status_code == 200:
+        log_str = f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}: Giá đã cập nhật thành công; Price = {_target_price}; Pricemin = {min_price}, Pricemax = {max_price}, GiaSosanh = {_current_top_price} - Seller: {_current_top_seller}"
+        log_str += f"\n----Top Sellers----\n- 1st Seller: {_current_top_seller} - Price: {_current_top_price}"
+        if (_second_current_top_seller is not None) and (_second_current_top_price is not None):
+            log_str += f"\n- 2nd Seller: {_second_current_top_seller} - Price: {_second_current_top_price}"
+        if (_third_current_top_seller is not None) and (_third_current_top_price is not None):
+            log_str += f"\n- 3rd Seller: {_third_current_top_seller} - Price: {_third_current_top_price}"
+        if (_fourth_current_top_seller is not None) and (_fourth_current_top_price is not None):
+            log_str += f"\n- 4th Seller: {_fourth_current_top_seller} - Price: {_fourth_current_top_price}"
+        log_data.append((index, log_str, 'C'))
 
     # log_str = f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}: Giá đã cập nhật thành công; Price = {_target_price}; Pricemin = {min_price}, Pricemax = {max_price}, GiaSosanh = {_current_top_price} - Seller: {_current_top_seller}"
     print(log_str)
